bff9b5dfa4415e412cd76df180b458196c10f6a1
Jonathan Tebaldi <jonathan9tebaldi1@gmail.com>
2023-10-04 14:53:21 -0300

    Exibir mensagens ao usuario e obter os valores digitados no terminal

--- conta-banco/src/main/java/ContaTerminal.java
+++ conta-banco/src/main/java/ContaTerminal.java
@@ -6,7 +6,14 @@ public class ContaTerminal {
         Scanner sc = new Scanner(System.in);
         //Exibir as mensagens para o nosso usuário
         //Obter pela scanner os valores digitados no terminal
-        
+        System.out.println("Digite o número da agência:");
+        int numero = sc.nextInt();
+        System.out.println("Digite a agência: ");
+        String agencia = sc.nextLine();
+        System.out.println("Digite seu nome: ");
+        String nome = sc.nextLine();
+        System.out.println("Digite seu saldo: ");
+        double saldo = sc.nextDouble();
 
         //Exibir a mensagem conta criada
 
