0f0b3f719810fa8ecb50f2971a04f0a7f1a68209
Jonathan Tebaldi <jonathan9tebaldi1@gmail.com>
2023-10-06 12:43:27 -0300

    Imprimindo uma lista de candidatos selecionados

--- DesafioProcessoSeletivo/controle-candidatoss/src/main/java/candidatura/ProcessoSeletivo.java
+++ DesafioProcessoSeletivo/controle-candidatoss/src/main/java/candidatura/ProcessoSeletivo.java
@@ -4,9 +4,18 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        selecaoCandidatos();
+        imprimirSelecionados();
     }
 
+
+    static void imprimirSelecionados(){
+        String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO"};
+        System.out.println("Imprimindo a lista de candidatos informando o indice do elemento");
+        for(int indice = 0; indice < candidatos.length; indice++){
+            System.out.println("O candidato de n°" + (indice + 1) + " é " + candidatos[indice]);
+        }
+
+    }
     static void selecaoCandidatos(){
         String [] candidatos = {"FELIPE", "MARCIA", "JULIA", "PAULO", "AUGUSTO", "MONICA", "FABRICIO", "MIRELA", "DANIELA", "JONATHAN"};
 
